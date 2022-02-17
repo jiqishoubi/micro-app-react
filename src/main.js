24b63069d2947c3d616e33c785ea3d84bb4c4b86
jiqishoubi <437753880@qq.com>
2022-02-17 14:05:40 +0800

    监听子路由

--- src/main.js
+++ src/main.js
@@ -19,4 +19,23 @@ microApp.start({
     },
   },
 })
+
+function dataListener(data) {
+  console.log('来自子应用my-app的数据', data)
+}
+
+/**
+ * 绑定监听函数
+ * appName: 应用名称
+ * dataListener: 绑定函数
+ * autoTrigger: 在初次绑定监听函数时如果有缓存数据，是否需要主动触发一次，默认为false
+ */
+microApp.addDataListener('app1', dataListener)
+
+// // 解绑监听my-app子应用的函数
+// microApp.removeDataListener(appName: string, dataListener: Function)
+
+// // 清空所有监听appName子应用的函数
+// microApp.clearDataListener(appName: string)
+
 ReactDOM.render(<App />, document.getElementById('root-main'))
